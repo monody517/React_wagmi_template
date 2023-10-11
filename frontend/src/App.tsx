73bafc2aa6b6c5433c63d493a2187c786f0ed56b
monody517 <2737716714@qq.com>
2023-10-11 14:07:13 +0800

    useWriteContract

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { mainnet, polygon, goerli } from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import "@rainbow-me/rainbowkit/styles.css";
+import ConnectWallet from "./components/ConnectWallet";
 
 function App() {
   const { chains, publicClient } = configureChains(
@@ -25,7 +26,7 @@ function App() {
     <>
       <WagmiConfig config={config}>
         <RainbowKitProvider chains={chains}>
-          <text>111</text>
+          <ConnectWallet />
         </RainbowKitProvider>
       </WagmiConfig>
     </>
